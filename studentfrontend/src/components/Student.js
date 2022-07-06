f67b9cb4d5a64324bcd2e2d987fbc03b45ad24c0
tpemba100 <tpemba.96@gmail.com>
2022-07-05 20:50:15 -0400

    UPDATE READ ME

--- studentfrontend/src/components/Student.js
+++ studentfrontend/src/components/Student.js
@@ -36,42 +36,40 @@ export default function Student() {
 
   return (
     <Container>
-        <Paper elevation = {3} style={paperStyle}>
+      <Paper elevation = {3} style={paperStyle}>
             <h1 style={{color:"#1976d2"} }> <u style={{textDecoration: 'none'}}>Add Student</u></h1>
-    <Box
-      component="form"
-      sx={{
-        '& > :not(style)': { m: 1 },
-      }}
-      noValidate
-      autoComplete="off"
-    >
-      <TextField id="outlined-basic" label="Student Name" variant="outlined" fullWidth
-      value={name}
-      onChange={(e)=>setName(e.target.value)}
-      />
-      <TextField id="outlined-basic" label="Student Address" variant="outlined" fullWidth
-      value={address}
-      onChange={(e)=>setAddress(e.target.value)}
-      />
-      <Button variant="contained" onClick={handleClick}>
-        Submit </Button>
-    </Box>
-    </Paper>
+        <Box
+          component="form"
+          sx={{
+            '& > :not(style)': { m: 1 },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <TextField id="outlined-basic" label="Student Name" variant="outlined" fullWidth
+          value={name}
+          onChange={(e)=>setName(e.target.value)}
+          />
+          <TextField id="outlined-basic" label="Student Address" variant="outlined" fullWidth
+          value={address}
+          onChange={(e)=>setAddress(e.target.value)}
+          />
+          <Button variant="contained" onClick={handleClick}>
+            Submit </Button>
+        </Box>
+      </Paper>
       <h1>Students</h1>
-    <Paper elevation={3} style={paperStyle}>
+      <Paper elevation={3} style={paperStyle}>
         {students.map(student=>(
           <Paper elevation={6} style={{margin:"10px",padding:"15px", textAlign:"left"}} key={student.id}>
-            Id #: {student.id}<br/>
-            Name: {student.name}<br/>
-            Address: {student.address}
+          Id #: {student.id}<br/>
+          Name: {student.name}<br/>
+          Address: {student.address}
           </Paper>
         ))
-      }
+        }
 
-
-
-    </Paper>
+      </Paper>
     </Container>
   );
 }
